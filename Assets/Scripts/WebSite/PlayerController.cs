a4f875e7043b7ae12d702b777d6fc540c7451fa0
Onitsag <gasti.tim@gmail.com>
2022-06-16 12:10:42 +0200

    WebSite Test Canvas

--- Assets/Scripts/WebSite/PlayerController.cs
+++ Assets/Scripts/WebSite/PlayerController.cs
@@ -32,7 +32,7 @@ public class PlayerController : MonoBehaviour
     {
 
 
-        if (isSprinting)
+        if (isSprinting && forwardMove>0)
         {
             curSpeed = speed * 1.5f;
         }
